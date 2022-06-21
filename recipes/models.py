80e85dfbacdb5c9384b2895fda5f16051c82b195
pattytonyoneill <pattytonyoneill@gmail.com>
2022-06-21 12:52:54 +0000

    Update models to use FK for comment author

--- recipes/models.py
+++ recipes/models.py
@@ -52,8 +52,9 @@ class Ingredients(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Recipe, on_delete=models.CASCADE,
                              related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_edit", default=''
+    )
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
