854d9c6efeb18519237690a84d63a7b12d4b183a
pattytonyoneill <pattytonyoneill@gmail.com>
2022-06-21 12:53:41 +0000

    Add 'edit' and 'delete' functionality for comments

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from .models import Recipe, Ingredients
+from django.contrib import messages
+from .models import Recipe, Ingredients, Comment
 from .forms import RecipeForm, CommentForm
 
 
@@ -55,8 +56,7 @@ class RecipeDetail(View):
             liked = True
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
+            comment_form.instance.author = request.user
             comment = comment_form.save(commit=False)
             comment.post = recipe
             comment.save()
@@ -86,3 +86,29 @@ class RecipeLike(View):
         else:
             recipe.likes.add(request.user)
         return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+
+
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    comment_form = CommentForm(request.POST or None, instance=comment)
+
+    if request.method == "POST":
+        if comment_form.is_valid():
+            comment_form.save()
+            messages.success(request, "Commented updated!")
+            return redirect(reverse("recipe_detail", args=[comment.post.slug]))
+        messages.error(request, "Error. Please try again.")
+    template = "edit_comment.html"
+    context = {
+        "comment": comment,
+        "comment_form": comment_form,
+    }
+    return render(request, template, context)
+
+
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if comment.author == request.user:
+        comment.delete()
+        messages.success(request, "Commented deleted!")
+        return redirect(reverse("home"))
